30885ee67e8481857950c8ad41e5b5e84730214a
Stanislav Kruchinin <stanislav.kruchinin@gmail.com>
2009-05-23 01:07:50 +0400

    Variable for path to latex.nmk.cmd script

--- templates/doctor/nomake.cmd
+++ templates/doctor/nomake.cmd
@@ -1,23 +1,25 @@
 @echo off
 
+set latexnmk=call ..\..\include\latex.nmk.cmd
+
 if "%1"=="help" (
 :help
-	call ..\..\include\latex.nmk.cmd %1
+	%latexnmk% %1
 	echo   all        build DVI of autoref and thesis
 	echo   allpdf     build PDF of autoref and thesis
 goto :eof
 ) else if "%1"=="all" (
 :all
-	set target=thesis & call ..\..\include\latex.nmk.cmd dvi
-	set target=autoref & call ..\..\include\latex.nmk.cmd dvi
+	set target=thesis & %latexnmk% dvi
+	set target=autoref & %latexnmk% dvi
 	set target=
 goto :eof
 ) else if "%1"=="allpdf" (
 :allpdf
-	set target=thesis & call ..\..\include\latex.nmk.cmd pdf
-	set target=autoref & call ..\..\include\latex.nmk.cmd pdf
+	set target=thesis & %latexnmk% pdf
+	set target=autoref & %latexnmk% pdf
 	set target=
 goto :eof
 ) else (
-	call ..\..\include\latex.nmk.cmd %*
+	%latexnmk% %*
 )
--- templates/master/nomake.cmd
+++ templates/master/nomake.cmd
@@ -1,13 +1,15 @@
 @echo off
 
+set latexnmk=call ..\..\include\latex.nmk.cmd
+
 if "%1"=="all" (
 :all
-	call ..\..\include\latex.nmk.cmd dvi
+	%latexnmk% dvi
 goto :eof
 ) else if "%1"=="allpdf" (
 :allpdf
-	call ..\..\include\latex.nmk.cmd pdf
+	%latexnmk% pdf
 goto :eof
 ) else (
-	call ..\..\include\latex.nmk.cmd %*
+	%latexnmk% %*
 )
